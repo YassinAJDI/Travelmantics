4c6287fcf59f273041fdbde5d8f7e31b8374244e
Yassin AJDI <yassin.ajdi@gmail.com>
2019-08-03 12:10:55 +0100

    Setup navigation component

--- app/src/main/java/com/ajdi/yassin/travelmantics/HomeActivity.kt
+++ app/src/main/java/com/ajdi/yassin/travelmantics/HomeActivity.kt
@@ -3,10 +3,10 @@ package com.ajdi.yassin.travelmantics
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_home)
     }
 }
---
+++ app/src/main/java/com/ajdi/yassin/travelmantics/InsertFragment.kt
@@ -0,0 +1,25 @@
+package com.ajdi.yassin.travelmantics
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * @author Yassin ADI
+ *
+ */
+class InsertFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_insert_deal, container, false)
+    }
+
+
+}
